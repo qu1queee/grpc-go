777850995646ff781fda25fef8a61c0678907cef
encalada <encalada@de.ibm.com>
2023-10-19 11:18:29 +0200

    wip

--- examples/helloworld/greeter_server/main.go
+++ examples/helloworld/greeter_server/main.go
@@ -25,14 +25,29 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"google.golang.org/grpc/keepalive"
 )
 
 var (
 	port = flag.Int("port", 50051, "The server port")
 )
+
+var kaep = keepalive.EnforcementPolicy{
+	MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+	PermitWithoutStream: true,            // Allow pings even when there are no active streams
+}
+
+var kasp = keepalive.ServerParameters{
+	MaxConnectionIdle:     180 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      180 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 60 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,   // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               40 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+}
 
 // server is used to implement helloworld.GreeterServer.
 type server struct {
@@ -51,7 +66,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+	s := grpc.NewServer(grpc.KeepaliveEnforcementPolicy(kaep), grpc.KeepaliveParams(kasp))
 	pb.RegisterGreeterServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
